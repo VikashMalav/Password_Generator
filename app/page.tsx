4e894abd2f0dd40c251608e52657740d476a5051
VikashMalav <malavvicky99@gmail.com>
2023-09-05 14:49:39 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,94 @@
-import Image from 'next/image'
+"use client"
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function Home() {
+  const [password, setpassword] = useState('');
+  const [length, setLength] = useState(8);
+  const [spChar, setSpChar] = useState(false);
+  const [number, setNumber] = useState(false);
+
+  const copyRef = useRef(null);
+
+  useEffect(() => {
+    PasswordGenerator();
+  }, [length, number, spChar]);
+
+  const PasswordGenerator = useCallback(() => {
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (spChar) str += "!@#$%^&*()_+-=[]{}|\\;:'\",./<>?`~";
+    if (number) str += "0123456789";
+    let pass = '';
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+    setpassword(pass);
+  }, [length, spChar, number, setpassword]);
+
+  const copyHandler = () => {
+    window.navigator.clipboard.writeText(password)
+      .then(() => {
+        copyRef.current.select();
+      })
+      .catch((error) => {
+        console.error('Error copying text to clipboard:', error);
+      });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-400 to-indigo-600">
+      <h1 className="text-4xl font-semibold text-white p-4 mb-8 bg-blue-800 rounded-lg shadow-lg">
+        Password Generator
+      </h1>
+      <div className="bg-white rounded-lg shadow-md p-4  w-full">
+        {/* Larger container for the generator */}
+        <div className="p-6 bg-gray-100 rounded-lg">
+          <div className="flex flex-wrap space-y-2 md:space-y-0 md:space-x-4">
+            <input
+              className="flex-1 py-2 px-4 rounded-lg border focus:ring-indigo-400"
+              autoFocus
+              value={password}
+              ref={copyRef}
+              readOnly
             />
-          </a>
+            <button
+              className="w-full md:w-auto px-4 py-2 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-indigo-200 focus:outline-none mt-2 md:mt-0"
+              type="button"
+              onClick={copyHandler}
+            >
+              COPY
+            </button>
+          </div>
+          <div className="flex flex-wrap space-y-2 mt-4 items-center justify-center gap-4">
+            <label className="flex items-center space-x-1">
+              <input
+                className="form-range"
+                type="range"
+                onChange={(e) => setLength(e.target.value)}
+              />
+              <span className="text-gray-600">Length: {length}</span>
+            </label>
+            <label className="flex items-center space-x-1">
+              <input
+                type="checkbox"
+                className="form-checkbox text-indigo-600"
+                checked={number}
+                onChange={() => setNumber((prev) => !prev)}
+              />
+              <span className="text-gray-600">Number Allowed</span>
+            </label>
+            <label className="flex items-center space-x-1">
+              <input
+                type="checkbox"
+                className="form-checkbox text-indigo-600"
+                checked={spChar}
+                onChange={() => setSpChar((prev) => !prev)}
+              />
+              <span className="text-gray-600">Special Characters Allowed</span>
+            </label>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
