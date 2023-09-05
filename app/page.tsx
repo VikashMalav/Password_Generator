b63ef772dc1a16c934c2b60ee4e850f462ead12b
VikashMalav <malavvicky99@gmail.com>
2023-09-05 15:08:31 +0530

    adjust styling commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function Home() {
@@ -37,22 +37,22 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-400 to-indigo-600">
-      <h1 className="text-4xl font-semibold text-white p-4 mb-8 bg-blue-800 rounded-lg shadow-lg">
+      <h1 className="text-4xl md:text-5xl  font-semibold text-white p-4 mb-4 md:mb-6 lg:mb-8 bg-blue-800 rounded-lg shadow-lg">
         Password Generator
       </h1>
-      <div className="bg-white rounded-lg shadow-md p-4  w-full">
+      <div className="bg-white rounded-lg shadow-md p-4 md:w-1/2 w-full">
         {/* Larger container for the generator */}
-        <div className="p-6 bg-gray-100 rounded-lg">
+        <div className="p-4 md:p-6 bg-gray-100 rounded-lg">
           <div className="flex flex-wrap space-y-2 md:space-y-0 md:space-x-4">
             <input
-              className="flex-1 py-2 px-4 rounded-lg border focus:ring-indigo-400"
+              className="flex-1 py-2 px-4 md:py-3 md:px-5 xl:px-6 rounded-lg border focus:ring-indigo-400"
               autoFocus
               value={password}
               ref={copyRef}
               readOnly
             />
             <button
-              className="w-full md:w-auto px-4 py-2 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-indigo-200 focus:outline-none mt-2 md:mt-0"
+              className="w-full md:w-auto px-4 py-2 md:px-5 md:py-3 xl:px-6 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700 focus:ring-indigo-400 focus:ring-offset-2 focus:ring-offset-indigo-200 focus:outline-none mt-2 md:mt-0"
               type="button"
               onClick={copyHandler}
             >
@@ -66,26 +66,34 @@ export default function Home() {
                 type="range"
                 onChange={(e) => setLength(e.target.value)}
               />
-              <span className="text-gray-600">Length: {length}</span>
+              <span className="text-gray-600 text-xs md:text-sm xl:text-base">
+                Length: {length}
+              </span>
             </label>
-            <label className="flex items-center space-x-1">
-              <input
-                type="checkbox"
-                className="form-checkbox text-indigo-600"
-                checked={number}
-                onChange={() => setNumber((prev) => !prev)}
-              />
-              <span className="text-gray-600">Number Allowed</span>
-            </label>
-            <label className="flex items-center space-x-1">
-              <input
-                type="checkbox"
-                className="form-checkbox text-indigo-600"
-                checked={spChar}
-                onChange={() => setSpChar((prev) => !prev)}
-              />
-              <span className="text-gray-600">Special Characters Allowed</span>
-            </label>
+            <div>
+              <label className="flex items-center space-x-1">
+                <input
+                  type="checkbox"
+                  className="form-checkbox text-indigo-600"
+                  checked={number}
+                  onChange={() => setNumber((prev) => !prev)}
+                />
+                <span className="text-gray-600 text-xs md:text-sm xl:text-base">
+                  Number Allowed
+                </span>
+              </label>
+              <label className="flex items-center space-x-1">
+                <input
+                  type="checkbox"
+                  className="form-checkbox text-indigo-600"
+                  checked={spChar}
+                  onChange={() => setSpChar((prev) => !prev)}
+                />
+                <span className="text-gray-600 text-xs md:text-sm xl:text-base">
+                  Special Characters Allowed
+                </span>
+              </label>
+            </div>
           </div>
         </div>
       </div>
